06d965c32c31f0a986e7f67d6d131f9b6480fcc8
LZC <linzhenchao@LZCMacBook-Pro.local>
2025-07-21 17:38:22 +0800

    feat: 添加项目骨架

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+{\rtf1\ansi\ansicpg936\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 export default function Home() \{\
+  return <h1 className="p-8 text-2xl">POD MVP \uc0\u19978 \u32447 \u25104 \u21151 \u65281 \u19979 \u19968 \u27493 \u65306 \u37197 \u32622  Supabase</h1>\
+\}}
